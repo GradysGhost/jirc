382eb6224d14b9637259f79699eac8d5dcc50fe6
Ryan Jung <ryan.jung@rackspace.com>
2012-10-02 10:17:54 -0500

    Added a bunch of function to logger.js

--- plugins/logger.js
+++ plugins/logger.js
@@ -1,20 +1,72 @@
 plugins.logger = {
 	streams : {},
-	message : function(client, from, channel, text, message) {
-		debugger;
+	
+	checkForStream : function(channel) {
 		var chan = channel.replace(/\W/, '');
 		if (!plugins.logger.streams["s" + chan]) {
-			plugins.logger.streams["s" + chan] = fs.createWriteStream("./log/" + channel);
+			plugins.logger.streams["s" + chan] = fs.createWriteStream("./log/" + channel, {'flags':'a'});
 		}
-		
+	},
+	
+	log : function(channel, message) {
+		plugins.logger.checkForStream(channel);
+		var chan = channel.replace(/\W/, '');
 		var d = new Date();
 		plugins.logger.streams["s" + chan].write(
 			"[" + d.getMonth() + "-" + d.getDate() + "-" + d.getFullYear() +
 			" " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + 
-			"." + d.getMilliseconds() + "] " +
-			"<" + from + "> " + text + "\n", "utf8"
+			"." + d.getMilliseconds() + "] " + message + "\n", "utf8"
 		);
 	},
+	
+	message : function(client, from, channel, text, message) {
+		plugins.logger.log(channel, "<" + from + "> " + text);
+	},
+	
+	pm : function(client, nick, text, message) {
+		plugins.logger.log(nick, text);
+	},
+	
+	topic : function(client, channel, topic, nick, message) {
+		plugins.logger.log(channel, "Topic (set by " + nick + "): " + topic);
+	},
+	
+	join : function(client, channel, nick, message) {
+		plugins.logger.log(channel, nick + " joined the channel.");
+	},
+	
+	part : function(client, channel, nick, reason, message) {
+		plugins.logger.log(channel, nick + " left the channel.");
+	},
+	
+	quit : function(client, nick, reason, channels, message) {
+		plugins.logger.log(channel, nick + " quit: \"" + reason + "\"");
+	},
+	
+	kick : function(client, channel, nick, by, reason, message) {
+		plugins.logger.log(channel, by + " kicked " + nick + ". \"" + reason + "\"");
+	},
+	
+	kill : function(client, nick, reason, channels, message) {
+		plugins.logger.log(channel, nick + " was killed. \"" + reason + "\"");
+	},
+	
+	nick : function(client, oldNick, newNick, channels, message) {
+		plugins.logger.log(channel, oldNick + " is now known as " + newNick);
+	},
+	
+	invite : function(client, channel, from, message) {
+		plugins.logger.log(channel, from + " invited you to " + channel);
+	},
+	
+	modeAdded : function(client, channel, by, mode, argument, message) {
+		plugins.logger.log(channel, by + " set your mode to " + mode);
+	},
+	
+	modeRemoved : function(client, channel, by, mode, argument, message) {
+		plugins.logger.log(channel, by + " set your mode to " + mode);
+	},
+	
 	shutdown : function() {
 		for (var s in plugins.logger.streams) {
 			plugins.logger.streams[s].end();
