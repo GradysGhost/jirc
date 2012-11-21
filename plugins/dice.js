560a53ca60f94e530e61942b463a16c45a738f33
Ryan Jung <ryan.jung@rackspace.com>
2012-11-21 14:17:33 -0600

    Why are dice so problematic?

--- plugins/dice.js
+++ plugins/dice.js
@@ -3,22 +3,24 @@ plugins.dice = {
 		if (debug) console.log(from + ", " + channel + ", " + text + ", " + message);
 		if (text.indexOf(config.commandChar + "dice") == 0) {
 			var parts = text.split(' ');
-			var matches = parts[1].match(/^\d+d\d+$/);
-			if (matches) {
-				var dice = matches[0].split('d')[0];
-				var sides = matches[0].split('d')[1];
-				if (debug) console.log("Dice: " + dice + "; Sides: " + sides);
-				var rolls = [];
-				if (dice <= 100 && sides <= 100) {
-					for (var i = 1; i <= dice; ++i) {
-						rolls.push(Math.ceil(Math.random() * sides));
+			if (parts.length > 1) {
+				var matches = parts[1].match(/^\d+d\d+$/);
+				if (matches) {
+					var dice = matches[0].split('d')[0];
+					var sides = matches[0].split('d')[1];
+					if (debug) console.log("Dice: " + dice + "; Sides: " + sides);
+					var rolls = [];
+					if (dice <= 100 && sides <= 100) {
+						for (var i = 1; i <= dice; ++i) {
+							rolls.push(Math.ceil(Math.random() * sides));
+						}
+						say(client, channel, rolls.toString());
+					} else {
+						say(client, channel, "Sorry, I can roll no more than 100 dice with no more than 100 sides.");
 					}
-					say(client, channel, rolls.toString());
 				} else {
-					say(client, channel, "Sorry, I can roll no more than 100 dice with no more than 100 sides.");
+					say(client, channel, "Command must be in the form of: " + config.commandChar + "dice <dice>d<sides>");
 				}
-			} else {
-				say(client, channel, "Command must be in the form of: " + config.commandChar + "dice <dice>d<sides>");
 			}
 		}
 	},
